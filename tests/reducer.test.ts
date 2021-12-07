eab6fb8aeed5178d7de5332e67e489fad48d5f81
elzup <guild0105@gmail.com>
2021-12-07 14:16:20 +0900

    fix detected logic

--- tests/reducer.test.ts
+++ tests/reducer.test.ts
@@ -48,13 +48,26 @@ Object {
   })
 
   test('suspended do not notice dups', () => {
-    expect(reducer({ noticed: true, lastMoved: hm0000 }, notDetected, hm0031)).
-toMatchInlineSnapshot(`
+    expect(reducer({ noticed: true, lastMoved: hm0000 }, notDetected, hm0031))
+      .toMatchInlineSnapshot(`
 Object {
   "doNotice": false,
   "state": Object {
     "lastMoved": 947462400000,
     "noticed": true,
+  },
+}
+`)
+  })
+
+  test('noticed reset with move detected', () => {
+    expect(reducer({ noticed: true, lastMoved: hm0000 }, detected, hm0029))
+      .toMatchInlineSnapshot(`
+Object {
+  "doNotice": false,
+  "state": Object {
+    "lastMoved": 947463600000,
+    "noticed": false,
   },
 }
 `)
